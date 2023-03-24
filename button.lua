199cebe8e7ca423818a580b1f5b7ec11f28d5706
JakeOJeff <87922888+JakeOJeff@users.noreply.github.com>
2023-03-24 19:59:16 +0530

    Made it compact and fit for menu
    Added Images for buttons instead of rectangles, Minor Bugs.

--- button.lua
+++ button.lua
@@ -1,23 +1,25 @@
 local class = require("middleclass")
 button = class("button")
 local buttons = {}
-local originalFont = love.graphics.getFont()
-sampleCodeVal = 0
+lg = love.graphics
 
-function button:initialize(text, code, x, heightS, rx, ry, textColor, font, color, hover, click)
+
+local normal = lg.newImage('world/myButtNormal.png')
+local hover = lg.newImage('world/myButtHover.png')
+local click = lg.newImage('world/myButtClick.png')
+
+function button:initialize(text, code, x, y)
 
 	self.code = code or (function() sampleCode = "hello" sampleCodeVal = sampleCodeVal + 1 end)
 	self.text = text or "Button"
 	self.x = x or 19
-	self.height = heightS or 25
-	self.rx = rx or 0
-	self.ry = ry or 0
-	self.textColor = textColor or {255,255,255}
-	self.font = font or love.graphics.getFont()
-	self.color = color or {0.5,0.5,0.5}
-	self.hover = hover or {0.4,0.4,0.4}
-	self.click = click or {0.7,0.7,0.7}
-	self.Fcolor = {self.color[1],self.color[2],self.color[3]}
+	self.y = y or 25
+
+	self.font = love.graphics.getFont()
+	self.width = normal:getWidth()
+	self.height = normal:getHeight()
+
+	self.image = normal
 	table.insert(buttons, self)
 	return self
 
@@ -26,34 +28,23 @@ end
 
 function button:update()
 	local x, y = love.mouse.getPosition()
-	if x < self.x + self.font:getWidth(self.text) + 20 and x > self.x and y < self.height + self.font:getHeight(self.text) + 20 and y > self.height then 
-		
+	if x < self.x + self.width and x > self.x and y < self.y + self.height and y > self.y then 
 		if not love.mouse.isDown(1) then
-			self.Fcolor = {self.hover[1],self.hover[2],self.hover[3]}
-	    else
-			self.code()
-			self.Fcolor = {self.click[1],self.click[2],self.click[3]}
+			self.image = hover
+	        else
 			love.timer.sleep(0.2)
+			self.image = click
+			self.code()
 		end
 	else
-		self.Fcolor = {self.color[1],self.color[2],self.color[3]}
+		self.image = normal
 	end 
 end
 
 function button:draw()
-	love.graphics.setFont(self.font)
-
-	love.graphics.setColor(self.Fcolor)
-	love.graphics.rectangle("fill", self.x, self.height, self.font:getWidth(self.text) + 20, self.font:getHeight(self.text) + 20, self.rx, self.ry)
-
-	love.graphics.setColor(self.textColor)
-	love.graphics.print(self.text, self.x + 10, self.height + 10)
-	
-	love.graphics.setColor(255,255,255)
-	love.graphics.setFont(originalFont)
-    if sampleCode then
-	love.graphics.print(sampleCode.." x"..math.floor(sampleCodeVal), 10, 75)
-	end
+	lg.draw(self.image,self.x,self.y)
+	love.graphics.print(self.text, self.x + self.width/2 - (self.font:getWidth(self.text)/2) - 4, self.y + self.height/2 - (self.font:getHeight(self.text)/2))
+	love.graphics.setColor(1,1,1)
 end 
 
 function update_buttons()
